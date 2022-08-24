ee9b42bb3374391a4789531bce2bdc1c7abe2741
Vinicius Campelo <vinibcampelo@gmail.com>
2022-08-23 23:50:16 -0300

    add class NoStackTraceException

---
+++ domain/src/main/java/com/fullcycle/admin/catolog/domain/exceptions/NoStackTraceException.java
@@ -0,0 +1,12 @@
+package com.fullcycle.admin.catolog.domain.exceptions;
+
+public class NoStackTraceException extends RuntimeException {
+
+    public NoStackTraceException(final String message) {
+        this(message, null);
+    }
+
+    public NoStackTraceException(final String message, final Throwable cause) {
+        super(message, cause, true, false);
+    }
+}
